1b2e66f453a28292446128e59be7d11715e0ccf5
hotgoddess <aalaserna@up.edu.ph>
2023-12-20 23:48:49 +0800

    Added WATER REFILL RESET

--- plantitspackage/Levels/Level.java
+++ plantitspackage/Levels/Level.java
@@ -2,12 +2,20 @@ package plantitspackage.Levels;
 import plantitspackage.*;
 
 public abstract class Level {
+    //ADDED THIS FOR WATER REFILL
+    private int WaterRefillCount = 2;
+    
     void purchaseItem(Items item) {
 
     }
 
     public boolean isItemAvailable(String itemType) {
         return false;
+    }
+
+    //ANDY ADDED THIS FOR WATER REFILL
+    public void refillWater(){
+        WaterRefillCount = 2;
     }
 
     public void printAvailaleItems() {
